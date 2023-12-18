7cc6a141849fd132cbdfe740abf34c63ce5b84ca
merkulovva <146440418+merkulovva@users.noreply.github.com>
2023-12-18 12:48:49 +1000

    3

---
+++ 3.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+class Human {
+
+private:
+    string name;
+    unsigned int height;
+    unsigned int weight;
+
+public:
+    Human(string n, unsigned int h, unsigned int w) {
+        name = n;
+        height = h;
+        weight = w;
+    }
+
+
+    string getName() {
+        return name;
+    }
+
+    unsigned int getHeight() {
+        return height;
+    }
+
+    unsigned int getWeight() {
+        return weight;
+    }
+
+    void setName(string n) {
+        name = n;
+    }
+
+    void setHeight(unsigned int h) {
+        height = h;
+    }
+
+    void setWeight(unsigned int w) {
+        weight = w;
+    }
+
+
+
+
+
+};
+
+
+
+class Granny : public Human {
+private:
+    unsigned int age;
+
+public:
+
+    Granny(string n, unsigned int h, unsigned int w, unsigned int age): Human(n, h, w) {
+        this->age = age;
+    }
+
+    unsigned int getAge() {
+        return age;
+    }
+
+};
+
+
+
+
+int main() {
+
+    Human chel1("Ivan", 195, 82);
+    Human chel2("Marina", 165, 55);
+    Human chel3("Oleg", 197, 94);
+    return 0;
+}
+
+
+
